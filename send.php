9461e79e0c6b93d7e02276d6b9f5beaa22779d16
Nikolay Dimitriev <dimitrievk56@gmail.com>
2020-10-01 14:59:55 +0300

    fix: outline to textarea, create: send for newsletter

--- send.php
+++ send.php
@@ -8,6 +8,7 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
+$email = $_POST['email'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
@@ -15,7 +16,8 @@ $body = "
 <h2>Новое обращение</h2>
 <b>Имя:</b> $name<br>
 <b>Телефон:</b> $phone<br><br>
-<b>Сообщение:</b><br>$message
+<b>Сообщение:</b><br>$message<br><br>
+<b>Почта:</b> $email
 ";
 
 // Настройки PHPMailer
